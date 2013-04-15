c375d27b0cbe3284f26219323d33768fe1975e62
Piotr Macuk <piotr@macuk.pl>
2013-04-15 15:28:25 +0200

    Client: validacje + to_s + testy

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ActiveRecord::Base
+  validates :name, :surname, :street, :city, length: { minimum: 2 }
+  validates :email, format: { with: /\A.+@.+\z/ }
+  validates :phone, format: { with: /\A\d{3}-\d{3}-\d{3}\z/ }
+  validates :postcode, format: { with: /\A\d{2}-\d{3}\z/ }
+  validates :street, :city, presence: true
+
+  def to_s
+    "#{name} #{surname} (#{id})"
+  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "init" do
+    c = Client.new
+    assert c.invalid?
+  end
+
+  test "name length" do
+    c = Client.new
+    c.valid?
+    assert c.errors[:name].any?
+    c.name = 'X'
+    c.valid?
+    assert c.errors[:name].any?
+    c.name = 'Ala'
+    c.valid?
+    assert c.errors[:name].empty?
+  end
+
+  test "email format" do
+    c = Client.new
+    c.valid?
+    assert c.errors[:email].any?
+    c.email = 'ala'
+    c.valid?
+    assert c.errors[:email].any?
+    c.email = 'ala@wp.pl'
+    c.valid?
+    assert c.errors[:email].empty?
+  end
+
+  test "phone format" do
+    c = Client.new
+    c.valid?
+    assert c.errors[:phone].any?
+    c.phone = '734'
+    c.valid?
+    assert c.errors[:phone].any?
+    c.phone = '504-234-334'
+    c.valid?
+    assert c.errors[:phone].empty?
+  end
 end
