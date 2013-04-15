87a96ff4073911235501add462a22febf340d7b0
Piotr Macuk <piotr@macuk.pl>
2013-04-15 15:42:06 +0200

    Account: validacje, relacje, testy

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @client = Client.new(name: 'Jan', surname: 'Kowalski',
+                         email: 'jan@wp.pl', phone: '345-678-334',
+                         street: 'Jasna 10', city: 'Warszawa',
+                         postcode: '00-110')
+    assert @client.save
+    @account = @client.accounts.create
+  end
+
+  test "set_number" do
+    assert @account.number.present?
+    assert @account.number > 1
+  end
+
+  test "to_s" do
+    assert_match /\[\d+\] 0 zł/, @account.to_s
+  end
+
+  test "balance" do
+    @account = @client.accounts.create
+    @account.balance = 'ala'
+    assert @account.invalid?
+    assert @account.errors[:balance].any?
+    @account.balance = 100
+    assert @account.valid?
+    assert @account.errors[:balance].empty?
+  end
 end
